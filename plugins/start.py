143ded99941d8fbcf82d71175786f109099b6e1f
Codeflix-Bots <159244479+Codeflix-Bots@users.noreply.github.com>
2024-04-17 14:46:39 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -161,7 +161,7 @@ async def start_command(client: Client, message: Message):
             verify_status = await get_verify_status(id)
             if IS_VERIFY and not verify_status['is_verified']:
                 short_url = f"api.shareus.io"
-                TUT_VID = f"{TUT_VID}"
+                TUT_VID = f"https://t.me/How_to_Download_7x/32"
                 token = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
                 await update_verify_status(id, verify_token=token, link="")
                 link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API,f'https://telegram.dog/{client.username}?start=verify_{token}')
