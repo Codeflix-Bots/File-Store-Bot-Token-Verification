61cdf60cbb0dea09337d2cfc85fbce65b21e11d1
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-08-24 11:49:35 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,14 +1,7 @@
-#(©)Codexbotz
-#rymme
-# https://www.youtube.com/@ultroidofficial
-
-
-
-
 from aiohttp import web
 
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("ultroid_official")
+    return web.json_response("codeflixbots")
